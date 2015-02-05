2dd23ef75eb489c71f608ab607b8faa6df93c555
ZhsCraftMounts <zhscraft@gmail.com>
2015-02-05 18:58:12 -0500

    added Main file

--- com/thejavanerds/mounts/Main.java
+++ com/thejavanerds/mounts/Main.java
@@ -1,13 +1,19 @@
 package com.thejavanerds.mounts;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Created by ZhsCraft on 2/5/2015.
  */
-public class Main {
+public class Main extends JavaPlugin{
+
+    public static Main plugin;
 
     public void onEnable()
     {
-
+        plugin = this;
+        Bukkit.getServer().getPluginManager().registerEvents(new Mount(plugin), this);
     }
 
     public void onDisable()
---
+++ com/thejavanerds/mounts/Mount.java
@@ -0,0 +1,14 @@
+package com.thejavanerds.mounts;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Created by ZhsCraft on 2/5/2015.
+ */
+public class Mount implements Listener{
+
+    public Main main;
+    public Mount(Main main){
+        this.main = main;
+    }
+}
