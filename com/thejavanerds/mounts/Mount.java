f5f90969316de3af3bf0d388b7b218277737b1be
ZhsCraftMounts <zhscraft@gmail.com>
2015-02-05 19:00:32 -0500

    added Main file

--- com/thejavanerds/mounts/Mount.java
+++ com/thejavanerds/mounts/Mount.java
@@ -1,14 +1,24 @@
 package com.thejavanerds.mounts;
 
+import org.bukkit.entity.Horse;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
 
 /**
  * Created by ZhsCraft on 2/5/2015.
  */
-public class Mount implements Listener{
+public class Mount implements Listener {
 
     public Main main;
-    public Mount(Main main){
+
+    public Mount(Main main) {
         this.main = main;
+    }
+
+    @EventHandler
+    public void Dismount(VehicleEnterEvent e) {
+        Horse horse = (Horse) e.getVehicle();
+        horse.remove();
     }
 }
